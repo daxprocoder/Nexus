7a65637a061530ad67cd65dbfd540c86cee076a6
Dax <daxprocoder@gmail.com>
2024-05-11 13:43:48 +0530

    open folder fuction

--- src/javascripts/fileconfig/openFolder.file.js
+++ src/javascripts/fileconfig/openFolder.file.js
@@ -1,5 +1,5 @@
-
-
+import os from 'os'
+import fs from 'fs'
 
 function OpenFodler(){
 
@@ -11,4 +11,24 @@ function OpenFodler(){
 }
 
 
+function OpenFiles(){
+    //user which open only selected files.
+    
+    const filesNames = ['../test/test.txt','../test/test2.txt']
+    
+    filesNames.forEach(fileName =>{
+        fs.readFile(fileName,'utf8',(err,data)=>{
+            if(err){
+                console.log('contents is coming')
+                return;
+            }
+            console.log(data)
+        })
+    })
+}
+
+// OpenFiles()
+
+
+export {OpenFiles}
 export{OpenFodler}
